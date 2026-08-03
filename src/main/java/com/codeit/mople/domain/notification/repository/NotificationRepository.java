e027f2ba7776acf31d29d1fb65290ab7af4d0638
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-08-03 15:44:06 +0900

    feat: NotificationRepository에 커스텀 Repository 상속 추가(#69)

--- src/main/java/com/codeit/mople/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/codeit/mople/domain/notification/repository/NotificationRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+public interface NotificationRepository extends JpaRepository<Notification, UUID>, NotificationRepositoryCustom {
 
     @Query("SELECT n FROM Notification n JOIN FETCH n.receiver WHERE n.receiver.id = :receiverId ORDER BY n.createdAt DESC")
     List<Notification> findByReceiverIdOrderByCreatedAtDesc(@Param("receiverId") UUID receiverId);
