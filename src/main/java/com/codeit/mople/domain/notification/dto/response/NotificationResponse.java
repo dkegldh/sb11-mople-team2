bcfa52b4a84e045382ad1cf67a84ec6ec2569daf
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-08-03 15:54:05 +0900

    refactor: NotificationResponse에 ENtity-Dto변환 베 메소드 추가(#69)

--- src/main/java/com/codeit/mople/domain/notification/dto/response/NotificationResponse.java
+++ src/main/java/com/codeit/mople/domain/notification/dto/response/NotificationResponse.java
@@ -1,5 +1,6 @@
 package com.codeit.mople.domain.notification.dto.response;
 
+import com.codeit.mople.domain.notification.entity.Notification;
 import com.codeit.mople.domain.notification.entity.NotificationLevel;
 import java.time.Instant;
 import java.util.UUID;
@@ -13,4 +14,14 @@ public record NotificationResponse(
     NotificationLevel level
 ) {
 
+    public static NotificationResponse from(Notification notification) {
+        return new NotificationResponse(
+            notification.getId(),
+            notification.getCreatedAt(),
+            notification.getReceiver().getId(),
+            notification.getTitle(),
+            notification.getContent(),
+            notification.getLevel()
+        );
+    }
 }
