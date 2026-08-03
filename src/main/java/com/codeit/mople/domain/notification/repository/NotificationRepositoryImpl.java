81160353c60d3de810e13c35a608769fe7fb6705
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-08-03 15:41:37 +0900

    feat: 알림 커서 조회 QueryDsl 구현

---
+++ src/main/java/com/codeit/mople/domain/notification/repository/NotificationRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.codeit.mople.domain.notification.repository;
+
+import static com.codeit.mople.domain.notification.entity.QNotification.notification;
+
+import com.codeit.mople.domain.notification.dto.request.NotificationCursorRequest;
+import com.codeit.mople.domain.notification.entity.Notification;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class NotificationRepositoryImpl implements NotificationRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Notification> findNotificationByCursor(UUID receiverId,
+        NotificationCursorRequest request, Instant cursorTime) {
+        return queryFactory
+            .selectFrom(notification)
+            .leftJoin(notification.receiver).fetchJoin()
+            .where(
+                notification.receiver.id.eq(receiverId),
+                cursorCondition(cursorTime, request.idAfter())
+            )
+            .limit(request.limit() + 1)
+            .orderBy(
+                notification.createdAt.desc(),
+                notification.id.desc()
+            )
+            .fetch();
+    }
+
+    private BooleanExpression cursorCondition(Instant cursorTime, UUID idAfter) {
+        if (cursorTime == null) {
+            return null;
+        }
+
+        if (idAfter != null) {
+            return notification.createdAt.lt(cursorTime)
+                .or(notification.createdAt.eq(cursorTime).and(notification.id.lt(idAfter)));
+        }
+
+        return notification.createdAt.lt(cursorTime);
+    }
+}
