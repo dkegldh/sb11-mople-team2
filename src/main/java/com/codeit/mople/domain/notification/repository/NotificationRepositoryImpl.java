51318239cdd7becc822b1db5ffe7f02571d81cb1
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-08-04 16:33:54 +0900

    refactor: 도절대  절대 성립되지 ã 않는 분기 제거

--- src/main/java/com/codeit/mople/domain/notification/repository/NotificationRepositoryImpl.java
+++ src/main/java/com/codeit/mople/domain/notification/repository/NotificationRepositoryImpl.java
@@ -2,7 +2,6 @@ package com.codeit.mople.domain.notification.repository;
 
 import static com.codeit.mople.domain.notification.entity.QNotification.notification;
 
-import com.codeit.mople.domain.notification.dto.request.NotificationCursorRequest;
 import com.codeit.mople.domain.notification.entity.Notification;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -17,16 +16,16 @@ public class NotificationRepositoryImpl implements NotificationRepositoryCustom 
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<Notification> findNotificationByCursor(UUID receiverId,
-        NotificationCursorRequest request, Instant cursorTime) {
+    public List<Notification> findNotificationByCursor(UUID receiverId, Instant cursorTime,
+        UUID idAfter, int limit) {
         return queryFactory
             .selectFrom(notification)
             .leftJoin(notification.receiver).fetchJoin()
             .where(
                 notification.receiver.id.eq(receiverId),
-                cursorCondition(cursorTime, request.idAfter())
+                cursorCondition(cursorTime, idAfter)
             )
-            .limit(request.limit() + 1)
+            .limit(limit + 1)
             .orderBy(
                 notification.createdAt.desc(),
                 notification.id.desc()
@@ -38,12 +37,7 @@ public class NotificationRepositoryImpl implements NotificationRepositoryCustom 
         if (cursorTime == null) {
             return null;
         }
-
-        if (idAfter != null) {
-            return notification.createdAt.lt(cursorTime)
-                .or(notification.createdAt.eq(cursorTime).and(notification.id.lt(idAfter)));
-        }
-
-        return notification.createdAt.lt(cursorTime);
+        return notification.createdAt.lt(cursorTime)
+            .or(notification.createdAt.eq(cursorTime).and(notification.id.lt(idAfter)));
     }
 }
