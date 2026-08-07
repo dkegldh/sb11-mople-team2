4287ce58b8a7917b8d72091b96617f59e7dad360
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-08-07 16:27:34 +0900

    refactor: 특정 유저를 팔로우하고있는 팔로워들의 id조회

--- src/main/java/com/codeit/mople/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/codeit/mople/domain/follow/repository/FollowRepository.java
@@ -1,9 +1,12 @@
 package com.codeit.mople.domain.follow.repository;
 
 import com.codeit.mople.domain.follow.entity.Follow;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FollowRepository extends JpaRepository<Follow, UUID> {
 
@@ -12,4 +15,7 @@ public interface FollowRepository extends JpaRepository<Follow, UUID> {
   Optional<Follow> findByFolloweeIdAndFollowerId(UUID followeeId, UUID followerId);
 
   long countByFolloweeId(UUID followeeId);
+
+  @Query("SELECT f.follower.id FROM Follow f WHERE f.followee.id = :followeeId")
+  List<UUID> findFollowerIdsByFolloweeId(@Param("followeeId") UUID followeeId);
 }
