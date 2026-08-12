c01bcb81a9cd6d5cbd7d5e0b7dc349e80579a09f
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-08-12 15:52:23 +0900

    refactor: AsyncConfig의 거부정책을 CallerRunsPloicy로 변경

--- src/main/java/com/codeit/mople/global/config/AsyncConfig.java
+++ src/main/java/com/codeit/mople/global/config/AsyncConfig.java
@@ -1,6 +1,7 @@
 package com.codeit.mople.global.config;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +26,10 @@ public class AsyncConfig implements AsyncConfigurer {
         executor.setThreadNamePrefix("async-notification-");
         executor.setWaitForTasksToCompleteOnShutdown(true);
         executor.setAwaitTerminationSeconds(30);
+        // 큐(500)와 스레드(10)가 모두 소진돼 제출이 거부될 상황이면, 기본값(AbortPolicy)처럼
+        // RejectedExecutionException을 던져 작업을 통째로 유실시키는 대신 제출한 스레드가
+        // 직접 실행하게 한다. 그 스레드가 잠깐 느려지는 게 알림이 조용히 사라지는 것보다 낫다.
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
     }
