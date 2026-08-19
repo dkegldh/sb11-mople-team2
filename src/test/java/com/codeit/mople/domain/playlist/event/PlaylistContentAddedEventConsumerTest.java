6818ff39c7ad6a4e12ced0b20ae4111b66ee9b3e
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-08-19 17:53:29 +0900

    test: 구독자 알림 격리 및 멱등성 마커 트랜잭션 회귀 테스트 추가

--- src/test/java/com/codeit/mople/domain/playlist/event/PlaylistContentAddedEventConsumerTest.java
+++ src/test/java/com/codeit/mople/domain/playlist/event/PlaylistContentAddedEventConsumerTest.java
@@ -1,12 +1,16 @@
 package com.codeit.mople.domain.playlist.event;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
 import com.codeit.mople.domain.notification.entity.NotificationType;
 import com.codeit.mople.domain.notification.service.NotificationCreator;
 import com.codeit.mople.domain.playlist.service.PlaylistService;
+import com.codeit.mople.domain.user.exception.UserErrorCode;
+import com.codeit.mople.domain.user.exception.UserException;
 import com.codeit.mople.global.event.processed.ProcessedEventRepository;
 import java.time.Instant;
 import java.util.List;
@@ -73,6 +77,37 @@ class PlaylistContentAddedEventConsumerTest {
         "테스트 플레이리스트에 새 콘텐츠가 추가되었습니다.",
         NotificationType.PLAYLIST_CONTENT_ADDED
     );
+    verify(notificationCreator).createNotification(
+        subscriberB,
+        "구독한 플레이리스트에 새 콘텐츠가 추가되었습니다.",
+        "테스트 플레이리스트에 새 콘텐츠가 추가되었습니다.",
+        NotificationType.PLAYLIST_CONTENT_ADDED
+    );
+  }
+
+  @Test
+  @DisplayName("한 구독자의 알림 생성이 실패해도 예외를 전파하지 않고 나머지 구독자는 알림을 받는다")
+  void handle_success_continuesOtherSubscribers_whenOneNotificationFails() {
+    // given
+    UUID eventId = UUID.randomUUID();
+    PlaylistContentAddedMessage message = new PlaylistContentAddedMessage(
+        eventId, Instant.now(), playlistContentId, playlistId, contentId, "테스트 플레이리스트"
+    );
+
+    given(processedEventRepository.insertIfAbsent(eventId)).willReturn(1);
+    given(playlistService.getSubscriberIds(playlistId))
+        .willReturn(List.of(subscriberA, subscriberB));
+    willThrow(new UserException(UserErrorCode.USER_NOT_FOUND))
+        .given(notificationCreator).createNotification(
+            subscriberA,
+            "구독한 플레이리스트에 새 콘텐츠가 추가되었습니다.",
+            "테스트 플레이리스트에 새 콘텐츠가 추가되었습니다.",
+            NotificationType.PLAYLIST_CONTENT_ADDED
+        );
+
+    // when & then
+    assertThatCode(() -> eventConsumer.handle(message)).doesNotThrowAnyException();
+
     verify(notificationCreator).createNotification(
         subscriberB,
         "구독한 플레이리스트에 새 콘텐츠가 추가되었습니다.",
